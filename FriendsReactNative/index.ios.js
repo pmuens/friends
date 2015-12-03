e2dbaec57c470ad82b2d6ae0c8889d1a344c00d4
Philipp Muens <raute1337@gmx.de>
2015-12-03 09:30:21 +0100

    Add iOS application code
    Add code which connects the iOS application to the Meteor server (via DDP) and
    fetches the friends which are then displayed with the help of a ListView.

--- FriendsReactNative/index.ios.js
+++ FriendsReactNative/index.ios.js
@@ -7,46 +7,98 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var _ = require('lodash');
+var DDPClient = require('ddp-client');
+
 var FriendsReactNative = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => !_.isEqual(row1, row2),
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    var ddpClient = new DDPClient({url: 'ws://localhost:3000/websocket'});
+
+    ddpClient.connect(() => ddpClient.subscribe('friends'));
+
+    // observe the lists collection
+    var observer = ddpClient.observe('friends');
+    observer.added = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.friends)));
+    observer.changed = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.friends)));
+    observer.removed = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.friends)));
+  },
+
+  updateRows: function(rows) {
+    this.setState({
+     dataSource: this.state.dataSource.cloneWithRows(rows),
+     loaded: true,
+   });
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderFriend}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          Loading lists...
         </Text>
       </View>
     );
-  }
+  },
+
+  renderFriend: function(friend) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.firstName}>{friend.firstName}</Text>
+        <Text style={styles.lastName}>{friend.lastName}</Text>
+      </View>
+    );
+  },
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  firstName: {
+    flex: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  lastName: {
+    flex: 5,
+    fontSize: 18,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: 'white',
   },
 });
 
